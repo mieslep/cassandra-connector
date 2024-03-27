a251aa571362519f72339cc03c64d4cb3c1fff9d
Phil Miesle <phil.miesle@datastax.com>
2024-03-27 20:20:30 +0000

    incrementing version, licence

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cassandra_connector",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "cassandra-driver>=3.29.1",
